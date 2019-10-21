07e4775d90c7c8036eb1fafb36892d858edbb7ed
Wence Cajucom <cajucom.w@gmail.com>
2019-10-20 21:08:03 -0700

    fix footer logos

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+// import instagram from '../../logos/instagram.svg';
 
 import './Footer.css';
 class Footer extends Component {
     render() {
         return (
             <div className="FooterStyle">
-                <h2>Wence Cajucom, Footer</h2>
+                <a target="_blank" href="https://www.instagram.com/fatwence/">
+                    {/* <img src={instagram} className="Instagram-logo" alt="instagram logo" /> */}
+                    <i className="icon-social-instagram" alt="instagram logo" />
+                </a>
             </div>
+
         )
     }
 }
