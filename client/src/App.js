6f38e944645d0035fa4e44447bc235d58e9f1ecd
Wence Cajucom <cajucom.w@gmail.com>
2019-10-22 21:03:26 -0700

    created client directory, move appropriate files over

---
+++ client/src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom'
+import './App.css';
+
+import NavBar from './components/NavBar/NavBar.js';
+import About from './components/About/About.js';
+import Tech from './components/Tech/Tech.js';
+import JiuJitsu from './components/Jiujitsu/Jiujitsu.js';
+import PTA from './components/PTA/PTA.js';
+import Footer from './components/Footer/Footer.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="FixedTop">
+          <h1>
+            wence cajucom
+          </h1>
+          
+          <div className="App-navBar">
+            <NavBar />
+          </div>
+
+        </div>
+        
+        <div className="App-mainContent">
+            <Switch>
+              <Route exact path='/' component={About} />
+              <Route exact path='/tech/' component={Tech} />
+              <Route exact path='/jiujitsu/' component={JiuJitsu} />
+              <Route exact path='/PTA/' component={PTA} />
+            </Switch>
+          </div>
+          
+        <div className="Footer">
+            <Footer />
+        </div>
+          
+      </div>
+    );
+  }
+}
+
+export default App;
--- config/keys.js
+++
@@ -1,2 +0,0 @@
-module.exports = {
-mongodb+srv:cajucom:DfMj6xninpSre2SS@cluster0-syazv.mongodb.net/test?retryWrites=true&w=majority};
