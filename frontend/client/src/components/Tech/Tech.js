ae58b0edd76ec5a1bd20b29dfff348b28396e860
Wence Cajucom <cajucom.w@gmail.com>
2019-10-27 12:05:37 -0700

    edit Tech.js to build state.

--- frontend/client/src/components/Tech/Tech.js
+++ frontend/client/src/components/Tech/Tech.js
@@ -7,20 +7,21 @@ import Article from '../Article/Article.js';
 
 class Tech extends Component {
     state = {
-        blogPosts: [],
+        title: 'Blog Title',
+        content: 'Content Goes Here',
     }
 
-    componentDidMount() {
-        console.log('working...');
-        fetch('api/all')
-            .then(response => response.json())
-            .then(data => {
-                console.log('did data come back?', data);
-                this.setState({
-                    blogPosts: data.posts,
-                });
-            });
-    }
+    // componentDidMount() {
+    //     console.log('working...');
+    //     fetch('api/all')
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             console.log('did data come back?', data);
+    //             this.setState({
+    //                 blogPosts: data.posts,
+    //             });
+    //         });
+    // }
 
     render() {
         return (
@@ -28,18 +29,8 @@ class Tech extends Component {
                 <h2>Wence Cajucom</h2>
                 <p>full stack developer</p>
                 <p>(...in training)</p>
-                {
-                this.state.blogPosts.map((post, index) => (
-                    <Article title={post.title} image={post.image} key={index}>
-                    {
-                        post.content.map((text, index) => (
-                        <p key={index}>{text}</p>
-                        ))
-                    }
-                    <Link to={'/post/' + index}>Read more...</Link>
-                    </Article>
-                ))
-                }
+                <h3>{this.state.title}</h3>
+                <p>{this.state.content}</p>
             </div>
         );
     }
